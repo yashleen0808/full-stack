96f798b3c81584c5285d4e9352f7d1c404dbd30c
Yashleen Kaur <122520259+yashleen0808@users.noreply.github.com>
2025-02-25 14:38:52 +0530

    Add files via upload

---
+++ fullstack/Lec-9_typescript/index.js
@@ -0,0 +1,9 @@
+"use strict";
+let a;
+a = 10;
+console.log(a);
+let str = "Hello";
+function sum(a, b) {
+    return a + b;
+}
+console.log(sum("Hello", 9));
---
+++ fullstack/Lec-9_typescript/index.ts
@@ -0,0 +1,8 @@
+let a: number;
+a=10;
+console.log(a);
+let str:String="Hello";
+function sum(a:string,b:number):string{
+   return a+b;
+}
+console.log(sum("Hello",9));
